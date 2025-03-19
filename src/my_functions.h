063946bdb6ce76526ba0fa5d640a8cc632de1158
jaywyawhare <2005441@kiit.ac.in>
2025-03-19 15:01:09 +0530

    Added loss functions in header file

--- src/my_functions.h
+++ src/my_functions.h
@@ -1,11 +1,13 @@
-typedef struct {
+typedef struct
+{
     float *weights;
     float *biases;
     int input_size;
     int output_size;
 } DenseLayer;
 
-typedef struct {
+typedef struct
+{
     float dropout_rate;
 } DropoutLayer;
 
@@ -13,7 +15,7 @@ float elu(float x, float alpha);
 float relu(float x);
 float sigmoid(float x);
 float tanH(float x);
-float* softmax(float *x, int size);
+float *softmax(float *x, int size);
 float linear(float x);
 float leakyRelu(float x);
 
@@ -36,3 +38,11 @@ float SGD(float x, float y, float lr, float *w, float *b);
 float l1_l2(float *w, float *dw, float *w_l1, float *dw_l1, float *w_l2, float *dw_l2, float l1, float l2, int n);
 float l1(float x, float y, float lr, float w, float b, float v_w, float v_b, float s_w, float s_b, float beta1, float beta2, float epsilon);
 float l2(float x, float y, float lr, float w, float b, float v_w, float v_b, float s_w, float s_b, float beta1, float beta2, float epsilon);
+
+float rootMeanSquaredError(float *y, float *yHat, int n);
+float reduceMean(float *loss, int size);
+float meanSquaredError(float *y, float *yHat, int n);
+float meanAbsolutePercentageError(float *y, float *yHat, int n);
+float meanAbsoluteError(float *y, float *yHat, int n);
+float focalLoss(float *y, float *yHat, int n, float gamma);
+float binaryCrossEntropyLoss(float *yHat, float *y, int size);
