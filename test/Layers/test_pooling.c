cb3109c594b84df57177a61157d65753df71322d
jaywyawhare <2005441@kiit.ac.in>
2025-04-08 05:23:01 +0530

    Fix : Spell fixed

--- test/Layers/test_pooling.c
+++ test/Layers/test_pooling.c
@@ -8,45 +8,45 @@
 void test_pooling()
 {
     {
-        PollingLayer layer;
-        int ret = initialize_polling(&layer, 2, 2);
+        PoolingLayer layer;
+        int ret = initialize_pooling(&layer, 2, 2);
         assert(ret == CM_SUCCESS);
 
-        ret = compute_polling_output_size(8, 2, 2);
+        ret = compute_pooling_output_size(8, 2, 2);
         assert(ret == 4);
 
         float input[8] = {1, 3, 2, 5, 0, 1, 4, 2};
         float output[4];
-        ret = forward_polling(&layer, input, output, 8);
+        ret = forward_pooling(&layer, input, output, 8);
         assert(ret == 4);
         assert(fabs(output[0] - 2) < 1e-6);
         assert(fabs(output[1] - 3.5) < 1e-6);
         assert(fabs(output[2] - 0.5) < 1e-6);
         assert(fabs(output[3] - 3) < 1e-6);
 
-        ret = free_polling(&layer);
+        ret = free_pooling(&layer);
         assert(ret == CM_SUCCESS);
         printf("Pooling layer normal test passed\n");
     }
 
     {
         int ret;
-        PollingLayer layer;
-        ret = initialize_polling(&layer, 0, 2);
+        PoolingLayer layer;
+        ret = initialize_pooling(&layer, 0, 2);
         assert(ret == CM_INVALID_KERNEL_SIZE_ERROR);
 
-        ret = initialize_polling(&layer, 2, 0);
+        ret = initialize_pooling(&layer, 2, 0);
         assert(ret == CM_INVALID_STRIDE_ERROR);
 
-        ret = initialize_polling(&layer, 2, 2);
+        ret = initialize_pooling(&layer, 2, 2);
         assert(ret == CM_SUCCESS);
 
-        ret = compute_polling_output_size(-1, 2, 2);
+        ret = compute_pooling_output_size(-1, 2, 2);
         assert(ret == CM_INVALID_INPUT_ERROR);
 
-        ret = forward_polling(&layer, NULL, (float[4]){0}, 8);
+        ret = forward_pooling(&layer, NULL, (float[4]){0}, 8);
         assert(ret == CM_NULL_POINTER_ERROR);
-        ret = forward_polling(&layer, (float[8]){1, 2, 3, 4, 5, 6, 7, 8}, NULL, 8);
+        ret = forward_pooling(&layer, (float[8]){1, 2, 3, 4, 5, 6, 7, 8}, NULL, 8);
         assert(ret == CM_NULL_POINTER_ERROR);
 
         printf("Pooling layer edge case tests passed\n");
