3221753f38a90f3048b7b53c81e6566c91290eba
jaywyawhare <2005441@kiit.ac.in>
2025-05-01 11:17:31 +0530

    Removed preprocessing

--- include/Preprocessing/min_max_scaler.h
+++
@@ -1,8 +0,0 @@
-#ifndef MIN_MAX_SCALER_H
-#define MIN_MAX_SCALER_H
-
-#include "../Core/autograd.h"
-
-Node *min_max_scaler_tensor(Node *x);
-
-#endif
--- include/Preprocessing/standard_scaler.h
+++
@@ -1,8 +0,0 @@
-#ifndef STANDARD_SCALER_H
-#define STANDARD_SCALER_H
-
-#include "../Core/autograd.h"
-
-Node *standard_scaler_tensor(Node *x);
-
-#endif
